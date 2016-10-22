377cec5c4b645a59b058b67d28e5c2277ffea600
Francisco Castellanos <fvcastellanos@gmail.com>
2016-10-21 22:22:02 -0600

    V1: Add account type

--- Model/Domain/AccountType.cs
+++ Model/Domain/AccountType.cs
@@ -5,5 +5,14 @@ namespace BalanceApi.Model.Domain
     {
         public long id { get; set;  }
         public string name { get; set; }
+
+        public AccountType(long id, string name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public AccountType() {
+        
+        }
     }
 }
