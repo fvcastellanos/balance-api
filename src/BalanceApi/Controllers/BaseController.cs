4a4cd60f3924d729defed502a38a7b731deab0d4
fvcg2 <fvcg2@FVCG-LENOVO>
2017-01-08 20:45:32 -0600

    EN-23: Add NUnit test framework

---
+++ src/BalanceApi/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System;
+
+namespace BalanceApi.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+
+        protected IActionResult ForException(Exception ex)
+        {
+            return InternalServerError(ex.Message);
+        }
+        protected IActionResult InternalServerError(object payload)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError, payload);
+        }
+    }
+}
